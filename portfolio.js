a88f34b9cb98e5444fc83f1a302f32a72bb12b44
julie-mo <40784525+julie-mo@users.noreply.github.com>
2020-06-11 16:37:54 -0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,101 @@
+var width = $(window).width();
+
+$(document).ready(function () {
+    var oldwidth = $(window).width();
+    $(window).resize(function () {
+        var nw = $(window).width();
+        //compare new and old width   
+        if ((oldwidth > 1000 && nw <= 1000) || (oldwidth <= 1000 && nw > 1000)) {
+            location.reload();
+        }
+        oldwidth = nw;
+        console.log(nw);
+        console.log(oldwidth)
+    });
+    ;
+});
+
+function navbar(){
+    if ((width >= 1000)){
+        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            $("#header").css("background","#fff");
+            $("#header").css("color","#666666");
+            $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+            $("#header").css("padding","4vh 4vw");
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid rgb(234, 84, 79)");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });
+        }else{
+            $("#header").css("background","transparent");
+            $("#header").css("color","#fff");
+            $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
+            $("#header").css("padding","6vh 4vw");
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid #fff");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });
+        }
+    }
+}
+var script = document.createElement('script');
+window.onload = (function loadScript() {
+    script.type = 'text/javascript';
+
+    if (width <= 1000) {
+        script.src = 'particlescss1.js';    
+        script.id = 'particlescss1';
+
+    } else {
+        script.src = 'particlescss.js';
+        script.id = 'particlescss';
+    }
+    document.body.appendChild(script);
+})
+
+window.onresize = navbar;
+window.onscroll = navbar;
+
+
+function magnify(imglink){
+    $("#img_here").css("background",`url('${imglink}') center center`);
+    $("#magnify").css("display","flex");
+    $("#magnify").addClass("animated fadeIn");
+    setTimeout(function(){
+        $("#magnify").removeClass("animated fadeIn");
+    },800);
+}
+
+function closemagnify(){
+    $("#magnify").addClass("animated fadeOut");
+    setTimeout(function(){
+        $("#magnify").css("display","none");
+        $("#magnify").removeClass("animated fadeOut");
+        $("#img_here").css("background",`url('') center center`);
+    },800);
+}
+
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1650);
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({
+        scrollTop: $(hash).offset().top
+        }, 1800, function(){
+        window.location.hash = hash;
+       });
+       } 
+      });
+  });
+  
