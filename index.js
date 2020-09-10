7a4414df69af7f2b60a9ad60eb6c12672d9057c1
Julie Mo <juliemo@MacIntosh-3.hitronhub.home>
2020-09-09 23:17:10 -0700

    "changed bio img layout"

--- index.js
+++ index.js
@@ -58,25 +58,6 @@ window.onload = (function loadScript() {
 window.onresize = navbar;
 window.onscroll = navbar;
 
-
-function magnify(imglink){
-    $("#img_here").css("background",`url('${imglink}') center center`);
-    $("#magnify").css("display","flex");
-    $("#magnify").addClass("animated fadeIn");
-    setTimeout(function(){
-        $("#magnify").removeClass("animated fadeIn");
-    },800);
-}
-
-function closemagnify(){
-    $("#magnify").addClass("animated fadeOut");
-    setTimeout(function(){
-        $("#magnify").css("display","none");
-        $("#magnify").removeClass("animated fadeOut");
-        $("#img_here").css("background",`url('') center center`);
-    },800);
-}
-
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
